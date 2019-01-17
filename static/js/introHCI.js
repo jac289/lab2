d4a2f60768e985275be3d79dda5cf2aa00403be7
Jaclyn Chin <jac289@ucsd.edu>
2019-01-17 13:02:15 -0800

    added Javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,9 +1,13 @@
+ 
+	$("document").ready(function() {
+       initializePage();
+});
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
 	});
 	$("a.thumbnail").click(projectClick);
-	function projectClick(e){
+	function projectClick(e) {
 		e.preventDefault();
 		$(this).css("background-color", "#7fff00");
 	}
